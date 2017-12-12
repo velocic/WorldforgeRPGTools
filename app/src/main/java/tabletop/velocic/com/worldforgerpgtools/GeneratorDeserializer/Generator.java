fa814f8271fe289452194a77f179b3a7731b4cbc
velocic <velocicdev@gmail.com>
2017-12-12 00:29:12 -0500

    Add gson dependency + gson deserializer objects

---
+++ app/src/main/java/tabletop/velocic/com/worldforgerpgtools/GeneratorDeserializer/Generator.java
@@ -0,0 +1,7 @@
+package tabletop.velocic.com.worldforgerpgtools.GeneratorDeserializer;
+
+public class Generator
+{
+    public String Name;
+    public Table[] Table;
+}
--- app/src/main/java/tabletop/velocic/com/worldforgerpgtools/GeneratorSelectionFragment.java
+++ app/src/main/java/tabletop/velocic/com/worldforgerpgtools/GeneratorSelectionFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 
 public class GeneratorSelectionFragment extends android.support.v4.app.Fragment
 {
@@ -19,5 +20,32 @@ public class GeneratorSelectionFragment extends android.support.v4.app.Fragment
         View view = inflater.inflate(R.layout.fragment_generators, container, false);
 
         return view;
+    }
+
+    private class GeneratorSelectionAdapter extends BaseAdapter
+    {
+        @Override
+        public int getCount()
+        {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i)
+        {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i)
+        {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup)
+        {
+            return null;
+        }
     }
 }
