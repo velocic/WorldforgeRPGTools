e6805ff8cb23e8ae4ab090f4a4368be83de1fe85
velocic <velocicdev@gmail.com>
2017-12-19 23:58:27 -0500

    Working through logic for table result rolls, including nested table lookup

--- app/src/main/java/tabletop/velocic/com/worldforgerpgtools/GeneratorDeserializer/ResultRoller.java
+++ app/src/main/java/tabletop/velocic/com/worldforgerpgtools/GeneratorDeserializer/ResultRoller.java
@@ -2,14 +2,17 @@ package tabletop.velocic.com.worldforgerpgtools.GeneratorDeserializer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class ResultRoller
 {
     private GeneratorCategory rootGeneratorCategory;
+    private Random rng;
 
     public ResultRoller(GeneratorCategory rootGeneratorCategory)
     {
         this.rootGeneratorCategory = rootGeneratorCategory;
+        this.rng = new Random();
     }
 
     public List<ResultItem> generateResultSet(String fullyQualifiedGeneratorPath, int numRolls)
@@ -20,7 +23,53 @@ public class ResultRoller
         Generator randomTable = rootGeneratorCategory.getGeneratorFromFullPath(fullyQualifiedGeneratorPath, rootGeneratorCategory);
         int tableMaxRollValue = determineTableDiceRange(randomTable.getTable());
 
+        for (int i = 0; i < numRolls; ++i) {
+            int roll = 1 + rng.nextInt(tableMaxRollValue);
+
+            for (TableEntries tableEntry : randomTable.getTable()) {
+                boolean entryIsMatch = checkRollResult(roll, );
+
+                if (entryIsMatch) {
+                    //if entry has a SubtableReroll
+                        //recursively call this function, with override for
+                        //min and max roll, and pass the fully qualified path to
+                        //the generator specified in SubtableReroll
+
+                    //If a similar entry exists in resultSet, add 1 to its' count
+                    //of matches
+
+                    //otherwise, insert entry into the result set
+                }
+            }
+        }
+
         return null;
+    }
+
+    private boolean checkRollResult(int roll, TableEntries tableEntry)
+    {
+        String diceRange = tableEntry.getDiceRange();
+        int rangeSeparatorIndex = diceRange.indexOf("-");
+
+        //Entry contains a single value
+        if (rangeSeparatorIndex == -1) {
+            int targetValue = Integer.parseInt(diceRange);
+
+            if (roll == targetValue) {
+                return true;
+            }
+        }
+
+        //Entry contains a range of values. check if roll is between the range
+        //inclusive on both ends
+        int leftVal = Integer.parseInt(diceRange.substring(0, rangeSeparatorIndex));
+        int rightVal = Integer.parseInt(diceRange.substring(rangeSeparatorIndex + 1));
+
+        if (roll >= leftVal || roll <= rightVal) {
+            return true;
+        }
+
+        return false;
     }
 
     private int determineTableDiceRange(TableEntries[] tableItems)
