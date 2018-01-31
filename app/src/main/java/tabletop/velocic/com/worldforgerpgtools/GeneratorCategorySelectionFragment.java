b0e7cc87f2b929e54cfb26e904ebf62c5667c425
velocic <velocicdev@gmail.com>
2018-01-30 22:37:28 -0500

    Finish up draft logic for selecting path of new generator category creation. Needs a button to add a new category to the current category. Also need to add a menubar icon to the main screen to launch this fragment

--- app/src/main/java/tabletop/velocic/com/worldforgerpgtools/GeneratorCategorySelectionFragment.java
+++ app/src/main/java/tabletop/velocic/com/worldforgerpgtools/GeneratorCategorySelectionFragment.java
@@ -2,10 +2,16 @@ package tabletop.velocic.com.worldforgerpgtools;
 
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import tabletop.velocic.com.worldforgerpgtools.GeneratorDeserializer.GeneratorCategory;
 import tabletop.velocic.com.worldforgerpgtools.GeneratorDeserializer.GeneratorImporter;
@@ -17,7 +23,10 @@ public class GeneratorCategorySelectionFragment extends android.support.v4.app.F
 
     private static final String ARG_CATEGORY_PATH = "category_path";
 
+    private Button selectButton;
     private GridView gridView;
+    private TextView currentlySelectedCategoryText;
+    private GeneratorCategorySelectionAdapter gridViewAdapter;
 
     public static GeneratorCategorySelectionFragment newInstance(String categoryPath)
     {
@@ -39,7 +48,7 @@ public class GeneratorCategorySelectionFragment extends android.support.v4.app.F
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        View view = inflater.inflate(R.layout.fragment_generators, container, false);
+        View view = inflater.inflate(R.layout.fragment_generator_categories, container, false);
 
         Bundle fragmentArgs = getArguments();
         if (fragmentArgs != null) {
@@ -50,6 +59,136 @@ public class GeneratorCategorySelectionFragment extends android.support.v4.app.F
         GeneratorCategory rootCategory = generatorImporter.getRootGeneratorCategory();
         currentCategory = generatorImporter.getRootGeneratorCategory().getCategoryFromFullPath(currentCategoryName, rootCategory);
 
+        selectButton = (Button) view.findViewById(R.id.button_select_category);
+        selectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                onSelectButtonClicked();
+            }
+        });
+
+        currentlySelectedCategoryText = (TextView) view.findViewById(R.id.textview_currently_selected_category);
+        currentlySelectedCategoryText.setText(currentCategoryName);
+
+        gridView = (GridView) view.findViewById(R.id.generator_selection);
+        gridViewAdapter = new GeneratorCategorySelectionAdapter(rootCategory);
+        gridView.setAdapter(gridViewAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View gridItemView, int i, long l)
+            {
+                onGridItemClicked(gridItemView);
+            }
+        });
+
         return view;
+    }
+
+    private void onSelectButtonClicked()
+    {
+        /*
+        TODO:
+            - open modal dialog asking for your "generator name"
+            - pass it the string from this fragment containing the full generator path
+            - return from that dialog to the "create new generator" fragment, closing
+                the possibly many instances of this fragment between the modal and
+                that screen
+         */
+    }
+
+    private void onGridItemClicked(View view)
+    {
+        GeneratorCategoryViewHolder viewHolder = (GeneratorCategoryViewHolder) view.getTag();
+
+        Fragment subCategoryFragment = newInstance(viewHolder.getCategory().getAssetPath());
+        getActivity().getSupportFragmentManager().beginTransaction()
+            .replace(R.id.fragment_container, subCategoryFragment)
+            .addToBackStack(null)
+            .commit();
+    }
+
+    private class GeneratorCategorySelectionAdapter extends BaseAdapter
+    {
+        private GeneratorCategory currentCategoryNode;
+
+        public GeneratorCategorySelectionAdapter(GeneratorCategory currentCategoryNode)
+        {
+            this.currentCategoryNode = currentCategoryNode;
+        }
+
+        @Override
+        public int getCount()
+        {
+            return currentCategoryNode.getNumGenerators();
+        }
+
+        @Override
+        public Object getItem(int i)
+        {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i)
+        {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup)
+        {
+            GeneratorCategoryViewHolder viewHolder = null;
+
+            View currentView = view;
+            if (currentView == null) {
+                LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+                currentView = layoutInflater.inflate(R.layout.grid_item_generators_and_categories, viewGroup, false);
+
+                viewHolder = new GeneratorCategoryViewHolder(
+                    currentView,
+                    currentCategoryNode.getChildCategory(i)
+                );
+
+                currentView.setTag(viewHolder);
+
+                return currentView;
+            }
+
+            viewHolder = (GeneratorCategoryViewHolder) currentView.getTag();
+            viewHolder.bind(currentCategoryNode.getChildCategory(i));
+
+            return currentView;
+        }
+    }
+
+    private class GeneratorCategoryViewHolder
+    {
+        private ImageView categoryIcon;
+        private TextView categoryText;
+        private GeneratorCategory category = null;
+
+        public GeneratorCategoryViewHolder(View view, GeneratorCategory category)
+        {
+            this.category = category;
+            categoryIcon = (ImageView) view.findViewById(R.id.generators_and_categories_grid_item_icon);
+            categoryText = (TextView) view.findViewById(R.id.generators_and_categories_grid_item_text);
+
+            bind(category);
+        }
+
+        public void bind(GeneratorCategory category)
+        {
+            this.category = category;
+            this.categoryIcon.setImageResource(R.drawable.ic_select_generator_category);
+            this.categoryText.setText(category.getName());
+        }
+
+        public GeneratorCategory getCategory()
+        {
+            return category;
+        }
     }
 }
