4273d6e36234b833b1213b2828839239b43c1470
velocic <velocicdev@gmail.com>
2019-01-09 13:04:17 -0500

    Working on porting generator selection fragment

--- app/src/main/java/tabletop/velocic/com/worldforgerpgtools/GeneratorSelectionFragment.kt
+++ app/src/main/java/tabletop/velocic/com/worldforgerpgtools/GeneratorSelectionFragment.kt
@@ -1,15 +1,19 @@
 package tabletop.velocic.com.worldforgerpgtools
 
+import android.content.Context
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import tabletop.velocic.com.worldforgerpgtools.GeneratorDeserializer.GeneratorCategory
 import kotlinx.android.synthetic.main.fragment_generators.*
+import tabletop.velocic.com.worldforgerpgtools.GeneratorDeserializer.Generator
+import tabletop.velocic.com.worldforgerpgtools.GeneratorDeserializer.GeneratorImporter
 
 class GeneratorSelectionFragment : android.support.v4.app.Fragment() {
-    //TODO: relocate these as locals?
-    private val currentCategory: GeneratorCategory? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +26,14 @@ class GeneratorSelectionFragment : android.support.v4.app.Fragment() {
 
         val fragmentArgs = arguments
         val currentCategoryName = fragmentArgs?.getString(ARG_CATEGORY_PATH) ?: ""
+
+        val generatorImporter = GeneratorImporter.getInstance(view.context)
+        val rootCategory = generatorImporter.rootGeneratorCategory
+
+        //TODO: probably refactor getCategoryFromFull path to pass itself as base node
+        val currentCategory = rootCategory.getCategoryFromFullPath(currentCategoryName, rootCategory)
+
+        //TODO: set up recyclerview w/ gridlayoutmanager
     }
 
     companion object {
@@ -39,5 +51,80 @@ class GeneratorSelectionFragment : android.support.v4.app.Fragment() {
 
             return fragment
         }
+    }
+}
+
+private class GeneratorSelectionAdapter(
+    private val context: Context,
+    private val currentCategoryNode: GeneratorCategory?
+) : RecyclerView.Adapter<GeneratorOrCategoryViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GeneratorOrCategoryViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        //TODO: this differs from original java impl, but I think it's correct; verify in testing
+        val numCategories = currentCategoryNode?.numChildCategories ?: 0
+        val numGenerators = currentCategoryNode?.numGenerators ?: 0
+
+        return numCategories + numGenerators
+    }
+
+    override fun onBindViewHolder(holder: GeneratorOrCategoryViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return super.getItemViewType(position)
+    }
+}
+
+private class GeneratorOrCategoryViewHolder(
+    view: View,
+    private var generator: Generator?
+) : RecyclerView.ViewHolder(view), View.OnClickListener {
+    private var generatorOrCategoryIcon: ImageView = view.findViewById(R.id.generators_and_categories_grid_item_icon)
+    private var generatorOrCategoryText: TextView = view.findViewById(R.id.generators_and_categories_grid_item_text)
+    private var category: GeneratorCategory? = null
+
+    constructor(view: View, category: GeneratorCategory) : this(view, null) {
+        this.category = category
+    }
+
+    init {
+        //if generator != null, set the icon & text as from a category
+        //if instead category != null, set the icon & test as from a generator
+
+        setViewFromGeneratorOrCategory()
+    }
+
+    override fun onClick(v: View?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    fun bind(generator: Generator) {
+        this.generator = generator
+        this.category = null
+
+        setViewFromGeneratorOrCategory()
+    }
+
+    fun bind(category: GeneratorCategory) {
+        this.category = category
+        generator = null
+
+        setViewFromGeneratorOrCategory()
+    }
+
+    private fun setViewFromGeneratorOrCategory() {
+        if (generator != null) {
+            generatorOrCategoryIcon.setImageResource(R.drawable.ic_generate_results)
+            generatorOrCategoryText.text = generator?.name
+            return
+        }
+
+        generatorOrCategoryIcon.setImageResource(R.drawable.ic_select_generator_category)
+        generatorOrCategoryText.text = category?.name
     }
 }
